3dc09791d0416cda70c13f6286ed4bc687956104
Veeresh SM <veeresh.sm@zebra.com>
2022-02-09 18:33:50 +0530

    commiting html file and some extras

--- first_flask.py
+++ first_flask.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 def home():
     return "<h1>Hello Veeresh </h1>"
 
-@app.route('/info')
-def info():
-    return '<h1> hi i am {},I am {} old '.format('Veeresh','23')
+@app.route('/<name>')
+def info(name):
+    return '<h1> hi i am {},I am {} old '.format(name,'23')
 
-# @app.route('/info1')
-# def info1():
-#     return '<h1> hi i am {},I am {} old '.format('Mandakini','23')
+@app.route('/info1')
+def info1():
+    return '<h1> hi i am {},I am {} old '.format('Naruto','23')
 
 @app.route('/info/about')
 def abour():
-    return "<h1> The quarantine is going on<h1>"
+    return "<h1> The Lock-Down is going on<h1>"
 
 
 if __name__ == '__main__':
